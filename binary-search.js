23396c7d6630c5eee47d0b931761e6beaf0a0908
Mindy Nguyen <mindyn3@educbe.ca>
2022-09-15 10:27:57 -0600

    wewe

--- binary-search.js
+++ binary-search.js
@@ -1,16 +1,19 @@
-function binarySearch(array, searchTerm, start, end) {
-		if (start >= end){
-		let mid = start + Math.floor((end - start)/ 2);
-		
-		if (array[mid]== searchTerm){
-			return mid
-		}if (array[mid] > searchTerm){
-			return binarySearch(array, searchTerm, start, mid + 1, end)
-		}if(array[mid] < searchTerm){
-			return binarySearch(array, searchTerm, start, mid - 1, end)
-		}
+function binarySearch(array, searchTerm) {
 
+	let start = 0;
+	let end = array.length - 1;
+
+	while (start<=end){
+		let mid = Math.floor((start + end)/ 2);
+		
+		if (array[mid]===searchTerm){
+			return mid;
+		}else if (array[mid] > searchTerm){
+			start = mid + 1;
+		}else (array[mid] < searchTerm)
+			end = mid -1;
 	}
 }
+
 
 module.exports = binarySearch;
